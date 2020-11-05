b04f6acd74217cd52cccfb69021446f82f545161
Eugene <kozlovea8@gmail.com>
2020-11-05 21:01:45 +0300

    remove unused allocation

--- file_helpers.rb
+++ file_helpers.rb
@@ -1,5 +1,5 @@
 require_relative 'buffer'
-
+require 'pry'
 module FileHelpers
   BATCH_SIZE = 5
 
@@ -27,26 +27,33 @@ module FileHelpers
     batch = []
     buffers = files.map { |f| Buffer.new(f) }
 
-    buffers.select!{|b| !b.empty?}
-    while(!buffers.empty?)
-      values = buffers.map(&:get)
-      min = values.min
-
-      batch << min
-      buffers[values.index(min)].pop
+    while(!buffers.all?(&:empty?))
+      minimium_buffer = find_buffer_with_min(buffers)
+      batch << minimium_buffer.get
+      minimium_buffer.pop
 
       if batch.size == BATCH_SIZE
         write_to_file(result, batch)
         batch = []
       end
-
-      buffers.select!{|b| !b.empty?}
     end
 
     if batch.size > 0
       write_to_file(result, batch)
       batch = []
     end
+  end
+
+  def find_buffer_with_min(buffers)
+    min = nil
+    buffers.each do |buffer|
+      next if buffer.empty?
+
+      min = buffer unless min
+      min = buffer if min.get > buffer.get
+    end
+
+    min
   end
 end
 
